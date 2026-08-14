f63d1ff8b9a0fd62a4f0b66b6765fd3eb31fb172
Gabriel Normandio <gabrielnormandio51@gmail.com>
2026-08-14 11:10:19 -0300

    Referenciando os posts do usuário

--- src/main/java/br/com/gabriel/workshop_mongo/config/Instantiation.java
+++ src/main/java/br/com/gabriel/workshop_mongo/config/Instantiation.java
@@ -28,19 +28,22 @@ public class Instantiation implements CommandLineRunner {
         SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
         sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
 
+        userRepository.deleteAll();
+        postRepository.deleteAll();
+
         User maria = new User(null, "Maria Brown", "maria@gmail.com");
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
-
-        userRepository.deleteAll();
 
         userRepository.saveAll(Arrays.asList(maria, alex, bob));
 
         Post post1 = new Post(null, sdf.parse("21/03/2026"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
         Post post2 = new Post(null, sdf.parse("23/03/2026"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
 
-        postRepository.deleteAll();
         postRepository.saveAll(Arrays.asList(post1, post2));
+
+        maria.getPosts().addAll(Arrays.asList(post1, post2));
+        userRepository.save(maria);
 
     }
 }
